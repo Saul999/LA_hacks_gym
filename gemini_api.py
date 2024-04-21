7fa38a05a467902e943486f5a5ceb9d3ee37cdd8
Vincent Hoang <vhoang8@uci.edu>
2024-04-20 21:44:28 -0700

    Updated prompt to output a dictionary and have it written to a text file.

--- gemini_api.py
+++ gemini_api.py
@@ -33,11 +33,13 @@ muscles_input = ", ".join(muscles)
 #print(muscles_input)
 training = "power lifting"
 days = {
-    'Monday':0,
+    'Sunday': 0,
+    'Monday': 0,
     'Tuesday': 120,
-    'Wednsday': 120,
+    'Wednsday': 0,
     'Thursday': 120,
-    'Friday': 45
+    'Friday': 0,
+    'Saturday': 0
 }
 
 import google.generativeai as genai
@@ -58,11 +60,20 @@ AI_PROMPT = f"""
             Wednesday for {days['Wednsday']} mins, 
             Thursday for {days['Thursday']} mins,
             Friday for {days['Friday']} mins, using only these muscles groups {muscles_input}?
-            Output this data as if it were a csv file, and put this data at the very top of
-            your output. Row 0 will be {days.keys()}, and the rows after will be the exercises
-            with its respective sets and reps, and it will not include the muscle group being trained.
+            For the output, there will be 7 rows, with each row representing a day in {days.keys()}. Based
+            on the day, each row will have a series of exercises corresponding to that day and availability.
+            Each exercise will be in the format of: Exercise Name: X Sets of Y Reps. 
+            For example, row 1 would represent Sunday and if Sunday is not 0 minutes, the row could possibly look like this:
+            Exercise 1 Name: X Sets of Y Reps, Exercise 2 Name: X Sets of Y Reps, . . . , Exercise N Name: X Sets of Y Reps
             """
 
 response = model.generate_content(AI_PROMPT)
 
-print(response.text)
+#print(response.text)
+#print("done")
+
+with open('results.txt', 'w', encoding="utf-8") as file:
+    gemini_output = response.text
+    print(gemini_output)
+    print("done")
+    file.write(gemini_output)
